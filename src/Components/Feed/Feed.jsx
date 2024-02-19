b99e887d6b9556ad962cdfce5b52468ad250b531
IamNayanIslam <iamnayanislam@gmail.com>
2024-02-19 11:27:13 +0600

    feed component completed

---
+++ src/Components/Feed/Feed.jsx
@@ -0,0 +1,107 @@
+import "./Feed.css"
+
+const Feed = () => {
+  return (
+    <div className="feed">
+        <div className="card">
+            <img src="./assets/thumbnail1.png" alt="" />
+            <h2>Best channel to learn coding that help you to be a web developer</h2>
+            <h3>Greatstack</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src="./assets/thumbnail2.png" alt="" />
+            <h2>Best channel to learn coding that help you to be a web developer</h2>
+            <h3>Greatstack</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src="./assets/thumbnail3.png" alt="" />
+            <h2>Best channel to learn coding that help you to be a web developer</h2>
+            <h3>Greatstack</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src="./assets/thumbnail4.png" alt="" />
+            <h2>Best channel to learn coding that help you to be a web developer</h2>
+            <h3>Greatstack</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src="./assets/thumbnail5.png" alt="" />
+            <h2>Best channel to learn coding that help you to be a web developer</h2>
+            <h3>Greatstack</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src="./assets/thumbnail6.png" alt="" />
+            <h2>Best channel to learn coding that help you to be a web developer</h2>
+            <h3>Greatstack</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src="./assets/thumbnail7.png" alt="" />
+            <h2>Best channel to learn coding that help you to be a web developer</h2>
+            <h3>Greatstack</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src="./assets/thumbnail8.png" alt="" />
+            <h2>Best channel to learn coding that help you to be a web developer</h2>
+            <h3>Greatstack</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src="./assets/thumbnail1.png" alt="" />
+            <h2>Best channel to learn coding that help you to be a web developer</h2>
+            <h3>Greatstack</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src="./assets/thumbnail2.png" alt="" />
+            <h2>Best channel to learn coding that help you to be a web developer</h2>
+            <h3>Greatstack</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src="./assets/thumbnail3.png" alt="" />
+            <h2>Best channel to learn coding that help you to be a web developer</h2>
+            <h3>Greatstack</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src="./assets/thumbnail4.png" alt="" />
+            <h2>Best channel to learn coding that help you to be a web developer</h2>
+            <h3>Greatstack</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src="./assets/thumbnail5.png" alt="" />
+            <h2>Best channel to learn coding that help you to be a web developer</h2>
+            <h3>Greatstack</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src="./assets/thumbnail6.png" alt="" />
+            <h2>Best channel to learn coding that help you to be a web developer</h2>
+            <h3>Greatstack</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src="./assets/thumbnail7.png" alt="" />
+            <h2>Best channel to learn coding that help you to be a web developer</h2>
+            <h3>Greatstack</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        <div className="card">
+            <img src="./assets/thumbnail8.png" alt="" />
+            <h2>Best channel to learn coding that help you to be a web developer</h2>
+            <h3>Greatstack</h3>
+            <p>15k views &bull; 2 days ago</p>
+        </div>
+        
+    </div>
+  )
+}
+
+export default Feed
