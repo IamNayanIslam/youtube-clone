bcd7b214c77b3b2239e8628d4fe46ee266af0e88
IamNayanIslam <iamnayanislam@gmail.com>
2024-02-19 11:06:12 +0600

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import Sidebar from '../../Components/Sidebar/Sidebar'
+import './Home.css'
+
+const Home = ({sidebar}) => {
+  return (
+    <div>
+      <Sidebar sidebar={sidebar}/>
+    </div>
+  )
+}
+
+export default Home
