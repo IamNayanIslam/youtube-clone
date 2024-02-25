eafeb9fdc917a0cb28537836b79537f513a21841
IamNayanIslam <iamnayanislam@gmail.com>
2024-02-25 15:36:22 +0600

    recommended side video list component completed

---
+++ src/Components/Recommended/Recommended.jsx
@@ -0,0 +1,76 @@
+import "./Recommended.css";
+
+const Recommended = () => {
+  return (
+    <div className="recommended">
+        <div className="side-video-list">
+            <img src="/assets/thumbnail1.png" alt="" />
+            <div className="vid-info">
+                <h4>Best channel that help you to be a web developer</h4>
+                <p>GreatStack</p>
+                <p>199K Views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src="/assets/thumbnail2.png" alt="" />
+            <div className="vid-info">
+                <h4>Best channel that help you to be a web developer</h4>
+                <p>GreatStack</p>
+                <p>199K Views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src="/assets/thumbnail3.png" alt="" />
+            <div className="vid-info">
+                <h4>Best channel that help you to be a web developer</h4>
+                <p>GreatStack</p>
+                <p>199K Views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src="/assets/thumbnail4.png" alt="" />
+            <div className="vid-info">
+                <h4>Best channel that help you to be a web developer</h4>
+                <p>GreatStack</p>
+                <p>199K Views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src="/assets/thumbnail5.png" alt="" />
+            <div className="vid-info">
+                <h4>Best channel that help you to be a web developer</h4>
+                <p>GreatStack</p>
+                <p>199K Views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src="/assets/thumbnail6.png" alt="" />
+            <div className="vid-info">
+                <h4>Best channel that help you to be a web developer</h4>
+                <p>GreatStack</p>
+                <p>199K Views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src="/assets/thumbnail7.png" alt="" />
+            <div className="vid-info">
+                <h4>Best channel that help you to be a web developer</h4>
+                <p>GreatStack</p>
+                <p>199K Views</p>
+            </div>
+        </div>
+        <div className="side-video-list">
+            <img src="/assets/thumbnail8.png" alt="" />
+            <div className="vid-info">
+                <h4>Best channel that help you to be a web developer</h4>
+                <p>GreatStack</p>
+                <p>199K Views</p>
+            </div>
+        </div>
+        
+
+    </div>
+  )
+}
+
+export default Recommended
--- src/Pages/Video/Video.jsx
+++ src/Pages/Video/Video.jsx
@@ -1,11 +1,12 @@
 import PlayVideo from "../../Components/PlayVideo/PlayVideo"
+import Recommended from "../../Components/Recommended/Recommended"
 import "./Video.css"
 
 const Video = () => {
   return (
     <div className="play-container">
         <PlayVideo/>
-       
+       <Recommended/>
     </div>
   )
 }
